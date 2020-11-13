80db2d0462d57ba9384fdb8ede1809d7f9832acf
Marc Victor <marc.carvalho@inteligenciaweb.com.br>
2020-11-13 03:18:17 -0300

    initial visual structure for Profile Screen

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { Image, SafeAreaView, Text, View } from 'react-native';
+
+import markLogo from '../../assets/mark.png';
+import Button from '../../components/Button';
+import styles from './styles';
 
 const Profile = () => {
   return (
-    <SafeAreaView>
-      <Text> Profile Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}> Profile Screen</Text>
+      <View style={styles.profileArea}>
+        <Image
+          source={{
+            uri:
+              'https://avatars2.githubusercontent.com/u/25596926?s=460&u=da3961b56881521ab0a36daec5214456fde9fdd1&v=4',
+          }}
+          style={styles.profileImg}
+        />
+
+        <Text style={styles.textName}>Marc Victor</Text>
+        <Text style={styles.textProfileInfo}>marcpassarelli</Text>
+        <Text style={styles.textProfileInfo}>São José</Text>
+        <Text style={styles.textProfileInfo}>
+          Eu sou formado em engenharia da computação desde 2013
+        </Text>
+      </View>
+      <View style={styles.buttonArea}>
+        <Button text="Logout" transparent={true} />
+      </View>
     </SafeAreaView>
   );
 };
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -1,5 +1,19 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  title: { fontSize: 20, marginVertical: 20 },
+  profileArea: { flex: 1, alignItems: 'center', marginHorizontal: 20 },
+  profileImg: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+  },
+  textName: { fontSize: 16, marginVertical: 5 },
+  textProfileInfo: { fontSize: 14, marginVertical: 5, color: '#808080' },
+  buttonArea: {
+    width: '60%',
+  },
+});
 
 export default styles;
