6665ae953c5785dcc07d02557b7acedb4078918e
Marc Victor <marc.carvalho@inteligenciaweb.com.br>
2020-11-13 02:46:56 -0300

    inserted background image in the login screen and made adjustments in the existent components

--- src/screens/Login/TextInputWithTitle/styles.js
+++ src/screens/Login/TextInputWithTitle/styles.js
@@ -1,8 +1,14 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  titleTextInput: { marginBottom: 5 },
-  textInput: { borderWidth: 1, borderRadius: 5, height: 40, paddingLeft: 5 },
+  titleTextInput: { marginBottom: 5, fontWeight: 'bold', fontSize: 16 },
+  textInput: {
+    borderWidth: 1.5,
+    borderRadius: 5,
+    height: 40,
+    paddingLeft: 5,
+    backgroundColor: 'rgba(255,255,255,0.4)',
+  },
 });
 
 export default styles;
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,20 +1,15 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import {
-  Image,
-  View,
-  SafeAreaView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
+import { Image, View, SafeAreaView, Text, ImageBackground } from 'react-native';
 
 import gitHubLogo from '../../assets/logo.png';
 import markLogo from '../../assets/mark.png';
+import backgroundImg from '../../assets/background.jpg';
+
 import CustomButton from '../../components/Button';
+import TextInputWithTitle from './TextInputWithTitle';
 
 import styles from './styles';
-import TextInputWithTitle from './TextInputWithTitle';
 
 const Login = () => {
   const { navigate } = useNavigation();
@@ -24,21 +19,23 @@ const Login = () => {
   };
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.logoAreaTop}>
-        <Text style={styles.title}>Sign in to</Text>
-        <Image source={gitHubLogo} style={styles.logo} />
-      </View>
-      <View style={styles.formArea}>
-        <TextInputWithTitle title="Username or email" />
-        <TextInputWithTitle title="Password" />
-        <CustomButton
-          text="Login"
-          onPress={() => {
-            goToProfile();
-          }}
-        />
-      </View>
-      <Image source={markLogo} style={styles.logoBottom} />
+      <ImageBackground source={backgroundImg} style={styles.backgroundImg}>
+        <View style={styles.logoAreaTop}>
+          <Text style={styles.title}>Sign in to</Text>
+          <Image source={gitHubLogo} style={styles.logo} />
+        </View>
+        <View style={styles.formArea}>
+          <TextInputWithTitle title="Username or email" />
+          <TextInputWithTitle title="Password" secureTextEntry={true} />
+          <CustomButton
+            text="Login"
+            onPress={() => {
+              goToProfile();
+            }}
+          />
+        </View>
+        <Image source={markLogo} style={styles.logoBottom} />
+      </ImageBackground>
     </SafeAreaView>
   );
 };
