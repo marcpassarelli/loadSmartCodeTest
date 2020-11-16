9b0047c96c95f98d83713506800673783ad83a3d
Marc Victor <marc.carvalho@inteligenciaweb.com.br>
2020-11-15 22:37:18 -0300

    added function handleBeforeRemove to handle logout when leaving the screen or pressing button Logout

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,33 +1,73 @@
-import React from 'react';
-import { Image, SafeAreaView, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback, useEffect } from 'react';
+import {
+  Alert,
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../state/auth/actions';
 
-import markLogo from '../../assets/mark.png';
-import Button from '../../components/Button';
 import styles from './styles';
 
 const Profile = () => {
+  const userInfo = useSelector((state) => state.user.userInfo);
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  useEffect(() => navigation.addListener('beforeRemove', handleBeforeRemove), [
+    handleBeforeRemove,
+    navigation,
+  ]);
+
+  const handleBeforeRemove = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      // Prompt the user before leaving the screen
+      Alert.alert(
+        'Logout',
+        'Do you want to logout and go back to the login screen?',
+        [
+          { text: 'No', style: 'cancel', onPress: () => {} },
+          {
+            text: 'Logout',
+            style: 'destructive',
+            // If the user confirmed, then we dispatch the action blocked earlier with preventDefault
+            // This will continue the action that had triggered the removal of the screen
+            onPress: () => {
+              dispatch(logout());
+              navigation.dispatch(event.data.action);
+            },
+          },
+        ],
+      );
+    },
+    [navigation, dispatch],
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}> Profile Screen</Text>
       <View style={styles.profileArea}>
         <Image
           source={{
-            uri:
-              'https://avatars2.githubusercontent.com/u/25596926?s=460&u=da3961b56881521ab0a36daec5214456fde9fdd1&v=4',
+            uri: userInfo.avatar_url,
           }}
           style={styles.profileImg}
         />
 
-        <Text style={styles.textName}>Marc Victor</Text>
-        <Text style={styles.textProfileInfo}>marcpassarelli</Text>
-        <Text style={styles.textProfileInfo}>São José</Text>
-        <Text style={styles.textProfileInfo}>
-          Eu sou formado em engenharia da computação desde 2013
-        </Text>
+        <Text style={styles.textName}>{userInfo.name}</Text>
+        <Text style={styles.textProfileInfo}>{userInfo.login}</Text>
+        <Text style={styles.textProfileInfo}>{userInfo.location}</Text>
+        <Text style={styles.textProfileInfo}>{userInfo.bio}</Text>
       </View>
-      <View style={styles.buttonArea}>
-        <Button text="Logout" transparent={true} />
-      </View>
+      <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+        <Text>Logout?</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
